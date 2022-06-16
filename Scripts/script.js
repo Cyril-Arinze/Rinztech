267e44e301c7aa11acc06d5e6e0928f258721c87
Cyril Arinze <ejicyrilarinze@gmail.com>
2022-06-16 19:01:49 +0100

    final commit

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,6 +3,6 @@
     var modalForm=document.getElementById("login-form")
     modalForm.addEventListener("submit", function (evt) {
         evt.preventDefault()
-        document.querySelector(".modal-body").innerHTML="<h5>Oops!!! <br> Sorry, webpage is still under development</h5>"       
+        document.querySelector(".modal-body").innerHTML="<h4>Oops!!! <br> Sorry, webpage is still under development</h4>"       
     })
  }());
